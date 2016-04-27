13399ac51a68421f03fe08f49e6ab8208cee8f2d
Yaser Nicaragua <yanv1991@gmail.com>
2016-04-27 00:21:58 -0600

    added routing and styles

---
+++ app/heroes.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from 'angular2/core';
+import { Router } from 'angular2/router';
+
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+import {HeroDetailComponent} from './hero-detail.component';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+	styleUrls: ['app/heroes.component.css'],
+	directives: [HeroDetailComponent],
+
+})
+export class HeroesComponent implements OnInit {
+	selectedHero: Hero;
+	heroes: Hero[];
+
+	constructor(private _router: Router, private _heroService: HeroService) {
+	}
+
+	getHeroes() {
+		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+	}
+
+	ngOnInit() {
+		this.getHeroes();
+	}
+
+	onSelect(hero: Hero) { 
+		this.selectedHero = hero; 
+	}
+
+	gotoDetail() {
+		this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+	}
+
+}
+ 
