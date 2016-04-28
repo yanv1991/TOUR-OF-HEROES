3a4633a73c8e02cc53128dbb92f95ad3f2dd1677
Yaser Nicaragua <YNicaragua@paciolan.com>
2016-04-28 11:25:14 -0600

    added nav tag to the links

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,8 +8,10 @@ import { DashboardComponent } from './dashboard.component';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <a [routerLink]="['Dashboard']">Dashboard</a>
-    <a [routerLink]="['Heroes']">Heroes</a>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['app/app.component.css'],
